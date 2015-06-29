7e4f2d95e2fd1135d0fd53e0b6d8a59c520fa209
greenteamer <greenteamer@bk.ru>
2015-06-29 16:19:39 +0600

    некоторые изменения sitemap

--- webshop/catalog/sitemap.py
+++ webshop/catalog/sitemap.py
@@ -1,26 +1,47 @@
 from django.contrib.sitemaps import Sitemap
-from webshop.catalog.models import Product
+from webshop.catalog.models import Product, Category
 from webshop.pages.models import Page, Article
+from django.contrib import sitemaps
+from django.core.urlresolvers import reverse
 
 class ProductsSitemap(Sitemap):
-    changefreq = "never"
-    priority = 0.5
+    changefreq = "monthly"
+    priority = 0.7
 
     def items(self):
         return Product.objects.all()
 
 
 class PagesSitemap(Sitemap):
-    changefreq = "never"
-    priority = 0.5
+    changefreq = "monthly"
+    priority = 0.8
 
     def items(self):
         return Page.objects.all()
 
 
 class ArticlesSitemap(Sitemap):
-    changefreq = "never"
+    changefreq = "monthly"
     priority = 0.5
 
     def items(self):
         return Article.objects.all()
+
+
+class CategorySitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.8
+
+    def items(self):
+        return Category.objects.all()
+
+
+# class StaticViewSitemap(sitemaps.Sitemap):
+#     priority = 1
+#     changefreq = 'monthly'
+
+#     def items(self):
+#         return ['main',]
+
+#     def location(self, item):
+#         return reverse(item)
