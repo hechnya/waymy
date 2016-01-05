74dc86a26f056c16b6480629f55208cc071a6806
hechnya <hechnya@mail.ru>
2016-01-05 18:46:36 +0300

    настроили 404

--- webshop/views.py
+++ webshop/views.py
@@ -3,10 +3,30 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
 
 
-def file_not_found_404(request):
+def error404(request):
     """Представление для страницы которая не найдена"""
     page_title = _(u'Page Not Found')
-    return render_to_response(
-        '404.html', locals(), context_instance=RequestContext(request))
+
+    response = render_to_response('404.html', locals(), context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+# def error404(request):
+#
+#     # 1. Load models for this view
+#     #from idgsupply.models import My404Method
+#
+#     # 2. Generate Content for this view
+#     template = get_template('404.htm')
+#     context = Context({
+#         'message': 'All: %s' % request,
+#         })
+#
+#     # 3. Return Template for this view + Data
+#     return HttpResponse(content=template.render(context), content_type='text/html; charset=utf-8', status=404)
